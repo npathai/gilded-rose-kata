17aecffd801113f19c48d95e5ca7bbb697fa705a
Narendra Pathai <narendra.pathai@gmail.com>
2020-07-21 17:06:51 -0400

    Refactoring, created short methods to increment and decrement quality and sell in

--- src/main/java/org/npathai/GildedRose.java
+++ src/main/java/org/npathai/GildedRose.java
@@ -12,49 +12,51 @@ public class GildedRose {
         for (Item item : items) {
 
             if (item.name.equals("Aged Brie")) {
-                if (item.getQuality() < 50) {
-                    item.setQuality(item.getQuality() + 1);
-                }
-                item.setSellIn(item.getSellIn() - 1);
+                incrementQuality(item);
+                decrementSellIn(item);
                 if (item.getSellIn() < 0) {
-                    if (item.getQuality() < 50) {
-                        item.setQuality(item.getQuality() + 1);
-                    }
+                    incrementQuality(item);
                 }
                 continue;
             } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 continue;
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.getQuality() < 50) {
-                    item.setQuality(item.getQuality() + 1);
-                }
+                incrementQuality(item);
                 if (item.getSellIn() < 11) {
-                    if (item.getQuality() < 50) {
-                        item.setQuality(item.getQuality() + 1);
-                    }
+                    incrementQuality(item);
                 }
                 if (item.getSellIn() < 6) {
-                    if (item.getQuality() < 50) {
-                        item.setQuality(item.getQuality() + 1);
-                    }
+                    incrementQuality(item);
                 }
-                item.setSellIn(item.getSellIn() - 1);
+                decrementSellIn(item);
                 if (item.getSellIn() < 0) {
                     item.setQuality(0);
                 }
                 continue;
-            } else if (item.name.equals("Amul Milk")) {
-                if (item.getQuality() > 0) {
-                    item.setQuality(item.getQuality() - 1);
-                }
-                item.setSellIn(item.getSellIn() - 1);
+            } else {
+                decrementQuality(item);
+                decrementSellIn(item);
                 if (item.getSellIn() < 0) {
-                    if (item.getQuality() > 0) {
-                        item.setQuality(item.getQuality() - 1);
-                    }
+                    decrementQuality(item);
                 }
                 continue;
             }
+        }
+    }
+
+    private void decrementSellIn(Item item) {
+        item.setSellIn(item.getSellIn() - 1);
+    }
+
+    private void decrementQuality(Item item) {
+        if (item.getQuality() > 0) {
+            item.setQuality(item.getQuality() - 1);
+        }
+    }
+
+    private void incrementQuality(Item item) {
+        if (item.getQuality() < 50) {
+            item.setQuality(item.getQuality() + 1);
         }
     }
 
