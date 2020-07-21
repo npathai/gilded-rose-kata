cb633bdf37ecb5c5e14290726770de7e5ed9e7fd
Narendra Pathai <narendra.pathai@gmail.com>
2020-07-21 16:59:45 -0400

    Caught Backstage Passes and removed it outside the if else jungle

--- src/main/java/org/npathai/GildedRose.java
+++ src/main/java/org/npathai/GildedRose.java
@@ -28,6 +28,27 @@ public class GildedRose {
                 continue;
             }
 
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.getQuality() < 50) {
+                    item.setQuality(item.getQuality() + 1);
+                }
+                if (item.getSellIn() < 11) {
+                    if (item.getQuality() < 50) {
+                        item.setQuality(item.getQuality() + 1);
+                    }
+                }
+                if (item.getSellIn() < 6) {
+                    if (item.getQuality() < 50) {
+                        item.setQuality(item.getQuality() + 1);
+                    }
+                }
+                item.setSellIn(item.getSellIn() - 1);
+                if (item.getSellIn() < 0) {
+                    item.setQuality(0);
+                }
+                continue;
+            }
+
             if (!item.getName().equals("Aged Brie") && !item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.getQuality() > 0) {
                     if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
