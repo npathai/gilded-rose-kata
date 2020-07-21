205d6ca1eb9aca595e0eebdf7cec55d69c169833
Narendra Pathai <narendra.pathai@gmail.com>
2020-07-21 15:57:11 -0400

    Added tests for Sulfuras

--- src/test/java/org/npathai/GildedRoseTest.java
+++ src/test/java/org/npathai/GildedRoseTest.java
@@ -39,7 +39,8 @@ class GildedRoseTest {
                 "-1, 11, -2, 13",
                 "0, 48, -1, 50"
         })
-        public void afterSellDateDateQualityIncreasesTwiceAsFast(int sellIn, int quality, int expectedSellIn, int expectedQuality) {
+        public void afterSellDateDateQualityIncreasesTwiceAsFast(int sellIn, int quality, int expectedSellIn,
+                                                                 int expectedQuality) {
             gildedRose.addItem(new Item("Aged Brie", sellIn, quality));
             gildedRose.tick();
             assertThat(gildedRose.getItems().get(0).sellIn).isEqualTo(expectedSellIn);
@@ -68,6 +69,62 @@ class GildedRoseTest {
             gildedRose.tick();
             assertThat(gildedRose.getItems().get(0).sellIn).isEqualTo(expectedSellIn);
             assertThat(gildedRose.getItems().get(0).quality).isEqualTo(50);
+        }
+    }
+
+    @Nested
+    public class Sulfuras {
+
+        @ParameterizedTest
+        @CsvSource({
+                "5, 5",
+                "1, 1",
+        })
+        public void beforeSellDateDoesntAge(int sellIn, int expectedSellIn) {
+            gildedRose.addItem(new Item("Sulfuras, Hand of Ragnaros", sellIn, 10));
+            gildedRose.tick();
+            assertThat(gildedRose.getItems().get(0).sellIn).isEqualTo(expectedSellIn);
+            assertThat(gildedRose.getItems().get(0).quality).isEqualTo(10);
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "0, 0",
+                "-1, -1",
+        })
+        public void afterSellDateDoesntAge(int sellIn, int expectedSellIn) {
+            gildedRose.addItem(new Item("Sulfuras, Hand of Ragnaros", sellIn, 10));
+            gildedRose.tick();
+            assertThat(gildedRose.getItems().get(0).sellIn).isEqualTo(expectedSellIn);
+            assertThat(gildedRose.getItems().get(0).quality).isEqualTo(10);
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "10, 10",
+                "11, 11",
+                "0, 0",
+                "50, 50"
+        })
+        public void beforeSellDateQualityDoesntChange(int quality, int expectedQuality) {
+            gildedRose.addItem(new Item("Sulfuras, Hand of Ragnaros", 1, quality));
+            gildedRose.tick();
+            assertThat(gildedRose.getItems().get(0).sellIn).isEqualTo(1);
+            assertThat(gildedRose.getItems().get(0).quality).isEqualTo(expectedQuality);
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "10, 10",
+                "1, 1",
+                "0, 0",
+                "50, 50"
+        })
+        public void afterSellDateQualityDoesntChange(int quality, int expectedQuality) {
+            gildedRose.addItem(new Item("Sulfuras, Hand of Ragnaros", -1, quality));
+            gildedRose.tick();
+            assertThat(gildedRose.getItems().get(0).sellIn).isEqualTo(-1);
+            assertThat(gildedRose.getItems().get(0).quality).isEqualTo(expectedQuality);
         }
     }
 }
